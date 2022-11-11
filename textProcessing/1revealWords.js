28648e220db50b5b0b924558c8a55f80776e3e98
Misho <99179066+mistiliyanov@users.noreply.github.com>
2022-11-11 15:35:11 +0000

    Update 1revealWords.js

--- textProcessing/1revealWords.js
+++ textProcessing/1revealWords.js
@@ -1,6 +1,5 @@
 function revealWords(wordsToCheck, text) {
     let wordsToReplace = wordsToCheck.split(', ');
-    console.log(wordsToReplace);
     wordsToReplace.forEach(word => {
         while (text.includes(' ' + '*'.repeat(word.length) + ' ')){
             let newText = text.replace(' ' + '*'.repeat(word.length) + ' ', ' ' + word + ' ');
